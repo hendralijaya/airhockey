bd77f63d37a745d00635f94d70184fb2ed0b3a03
Hendra Lijaya <hendralijaya@gmail.com>
2024-06-02 22:26:31 +0700

    Initial Commit

---
+++ AirHockey/AirHockeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirHockeyApp.swift
+//  AirHockey
+//
+//  Created by hendra on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AirHockeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
