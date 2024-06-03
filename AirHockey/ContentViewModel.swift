7432b13f31e5cf4b6579ca0a2dee1a6aa907d3b8
Hendra Lijaya <hendralijaya@gmail.com>
2024-06-03 17:24:44 +0700

    Init Project

---
+++ AirHockey/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  AirHockey
+//
+//  Created by hendra on 02/06/24.
+//
+
+import Foundation
---
+++ AirHockey/GameState.swift
@@ -0,0 +1,8 @@
+//
+//  GameState.swift
+//  AirHockey
+//
+//  Created by hendra on 02/06/24.
+//
+
+import Foundation
